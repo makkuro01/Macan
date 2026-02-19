b9d0b4e27ec14f2003391a8ca702c6fca7c86cf4
akumaru <olyakonareva220@gmail.com>
2026-02-19 16:00:16 +0500

    first commit

---
+++ macanHuesos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.Pipes;
+
+namespace macanHuesos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka = "Я говрою макан, вы говорите хуесос";
+            Console.WriteLine(stroka);
+            while (true)
+            {
+               Console.WriteLine( "Макан ");
+                string answer = Console.ReadLine();
+                if(answer.ToLower() != "хуесос" || answer.ToLower() != "Xуесос")
+                {
+                    Console.WriteLine("Почему Макан не хуесос?");
+                    break;
+                }
+               
+            }
+            Console.WriteLine("Нажмите любую кнопку для выхода");
+            Console.ReadKey();
+        }
+    }
+}
